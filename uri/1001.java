ea7580469b655f7364aac4cc0a2e97ea9e59a81d
Leonardo Peres de Melo Goulart <lpgoulart@A202-06L.imd.local>
2018-03-01 17:58:36 -0300

    First problem solve in Java

---
+++ uri/1001.java
@@ -0,0 +1,18 @@
+// Leonardo Goulart
+// Java Programming language
+
+import java.util.Scanner;
+
+class uri1001 {
+ 
+    public static void main(String[] args){
+ 
+        Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		System.out.print( "X = " );
+        System.out.println( x + y );
+ 
+    }
+ 
+}
