44e1ba056a01407032b74c5d5e75006c0b7eb911
leonardo@roffor.com.br <leonardo@roffor.com.br>
2018-03-02 00:07:44 -0300

    simple sum solved

---
+++ uri/1003.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class uri1003 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		System.out.printf("SOMA = " + (x + y) + "\n");
+	}
+}
