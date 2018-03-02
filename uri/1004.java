03184f88250bd637f8ff01cee5957aed061d3d0c
leonardo@roffor.com.br <leonardo@roffor.com.br>
2018-03-02 08:03:54 -0300

    problem solved

---
+++ uri/1004.java
@@ -0,0 +1,17 @@
+// Leonardo Goulart
+// Java Programming language
+
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+      Scanner scan = new Scanner(System.in);
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		System.out.printf("PROD = " + (x * y) + "\n");
+ 
+    }
+ 
+}
