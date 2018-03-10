7cc11cadcf433dda331f1178cd660044e5800a30
leonardo@roffor.com.br <leonardo@roffor.com.br>
2018-03-09 23:15:11 -0300

    working FileMaker

--- makeFiles/FileMaker.java
+++ makeFiles/FileMaker.java
@@ -19,44 +19,42 @@ class FileMaker {
 			// Variable to read inputs from the user
 			Scanner scan = new Scanner(System.in);
 			// Strings variables
-			//**String path;
-			//**String fileName;
+			String path;
+			String fileName;
 			String line;
-			// Condition to stop the input from the user
-			boolean entry = true;
+			
 			//Path where the new file it's going to be
-			/*
-			*	System.out.println("Enter the path for new file: ");
-			* 	path = scan.nextLine();
-			*	System.out.println("Give a name for your file: ");
-			*	fileName = scan.nextLine();
-			*	path = path + fileName; //(need to test if this works first)
-			*/
-			String path = "Test.java";
+			System.out.println("Enter the path for new file: ");
+			path = scan.nextLine();
+			System.out.println("Give a name for your file: ");
+			fileName = scan.nextLine();
+			path = "../" + path + "/" + fileName + ".java"; //(need to test if this works first)
+			
 			// File creation and verifying if it doesn't exist. If so create a new
 			File file = new File(path);
 				if (!file.exists()) {
 				    file.createNewFile();
 				}
+			System.out.println("File created");
+
 			// List with the inputs
 			ArrayList<String> lista = new ArrayList<String>();
-
 			// This method Returns the absolute form of this abstract pathname. Equivalent to new File
 			FileWriter fw = new FileWriter(file.getAbsoluteFile());
 			// Writes text to a character-output stream
 			BufferedWriter bw = new BufferedWriter(fw);
-			// String to compare. condition to exit the input lines
-			String exit = "exit";
-			
-			// Get the inputs from user until exit
-			while (entry) {	
-				line = scan.nextLine();
-				if (line.equals(exit)) {
-					entry = false;
-				} else {
-					lista.add (line);
-				}
-			}
+
+			lista.add ("//Leonardo Goulart");
+			lista.add ("//UFRN");
+			lista.add ("//Java Programming language");
+			System.out.println("Name your class:");
+			line = scan.nextLine();
+			lista.add ("\n");
+			lista.add ("class " + line + " {\n");
+			lista.add ("	public static void main (String[] args) {\n");
+			lista.add ("	}");
+			lista.add ("}");
+
 			// Write the input lines on the new file one by one
 			do 
 			{
--- makeFiles/Test.java
+++
@@ -1,8 +0,0 @@
-// Leonardo Goulart
-// IT student - UFRN
-
-class Working {
-	public static void main(String[] args){
-		System.out.println("Working");
-	}
-}
